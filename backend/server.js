db53b3228fc19677369aabcd5f14df44380d01e8
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2023-11-13 21:04:27 +0500

    Auth Setup for Backend [Messy]

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
+require("./utils/auth");
+const passport = require("passport");
 const app = express();
 
 // Port for the server
@@ -16,7 +19,55 @@ app.use(express.urlencoded({ extended: false }));
 // The express.json() middleware is used to parse JSON data in the request body.
 app.use(express.json());
 
+function isLoggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+app.use(
+  session({
+    secret: "keyboardcat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // routes
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Something went wrong");
+});
+
+app.get("/auth/protected", isLoggedIn, (req, res) => {
+  let name = req.user.displayName;
+  res.send(`Hello ${name}!`);
+});
+
+app.use("/auth/logout", (req, res) => {
+  req.session.destroy();
+  res.send("You have logged out");
+});
+
 app.use("/test", require("./routes/test"));
 app.get("/", (req, res) => {
   res.send("Point of Sale");
